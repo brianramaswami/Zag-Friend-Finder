8ce644b2211ecc9cf2d7fd27c1da0b3c454ecf75
Timewizard15 <timewizard96@gmail.com>
2018-05-08 04:38:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
-from classes import ParseData
+import ParseData
 import csv
 import sys
 
 
 def Main():
-    while(exit != True):
+    count = 0
+    while (exit != True):
 
-        UserAnswer = input("\nWould you like to? :  \n(1): Display Current Data.\n(2): Create New User \n(3): Analyze Data \n(4): Exit. \n")
+        UserAnswer = input(
+            "\nWould you like to? :  \n(1): Display Current Data.\n(2): Create New User \n(3): Analyze Data \n(4): Exit. \n")
         Data = ParseData.readData();
 
-        if(UserAnswer == "1"):
-            
-            #READS IN DATA FROM CSV INTO NESTED ARRAY/LIST
-            #print('');
-            #print('READS IN DATA FROM CSV INTO NESTED ARRAY/LIST');
+        if (UserAnswer == "1"):
+
+            # READS IN DATA FROM CSV INTO NESTED ARRAY/LIST
+            # print('');
+            # print('READS IN DATA FROM CSV INTO NESTED ARRAY/LIST');
             Data = ParseData.readData();
 
-            #DISPLAYS NESTED LIST
-            #print('');
-            #print('DISPLAYS NESTED LIST');
+            # DISPLAYS NESTED LIST
+            # print('');
+            # print('DISPLAYS NESTED LIST');
             ParseData.showData(Data);
-        elif(UserAnswer == "2"):
-            createData = ParseData.createData(Data);
+        elif (UserAnswer == "2"):
+            count = count + 1
+            createData = ParseData.createData(Data, count);
 
-        elif(UserAnswer == "3"):
-            #MATT DO SOMETHING HERE
+        elif (UserAnswer == "3"):
+            # MATT DO SOMETHING HERE
             ParseData.analyzeData(Data);
 
-        elif(UserAnswer == "4"):
+        elif (UserAnswer == "4"):
             sys.exit();
 
         else:
             print("TRY AGAIN .. DUMMY \n");
+
 
 Main();
